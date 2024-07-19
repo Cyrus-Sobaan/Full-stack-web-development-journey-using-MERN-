d0e591db0abaf36b45ab276a2a020ebce790931b
Cyrus-Sobaan <sobaanqureshi1@gmail.com>
2024-07-20 00:10:20 +0530

    add some js files

--- basics/script.js
+++ basics/script.js
@@ -1,9 +1,35 @@
-const profile = {
-    userName: "sobaan",
-    isFollow:true,
-    followers: 234,
-    following:2342,
+// const profile = {
+//     userName: "sobaan",
+//     isFollow:true,
+//     followers: 234,
+//     following:2342,
 
 
-};
-console.log(typeof profile ["userName"]);
+// };
+// console.log(typeof profile ["userName"]);
+
+
+// Arithmatic operators
+
+let a = 5;
+let b = 2;
+
+
+// console.log("a =", a,  "& b =",b)
+// console.log("a+b =", a+b )
+// console.log("a-b =", a-b )
+// console.log("a*b =", a*b )
+// console.log("a/b =", a/b )
+// console.log("a**b=", a**b)
+
+//Unary operators
+// console.log("a++b=", a++ )answer:5 increament first then print to next line
+// console.log(a) answer:6
+// console.log("a++b=",++a) answer:6 because this is post increamnet print first 
+
+
+
+//Assignment operators
+
+// console.log("a+=b", a += b) it means a=a+1;
+// console.log("a-=b =", a-= b) means a=a-1
